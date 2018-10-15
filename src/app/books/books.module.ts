b890b6dce3767145b5cee02506188380b3717597
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-10-15 19:15:57 +0200

    added shared module

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { MaterialModule } from '../shared/material.module';
+import { SharedModule } from '../shared/shared.module';
 import { BooksRoutes } from './books.routing';
 import { BookDetailsComponent } from './container/book-details/book-details.component';
 import { BooksOverviewComponent } from './container/books-overview/books-overview.component';
@@ -14,18 +14,18 @@ import { BookListComponent } from './presentational/book-list/book-list.componen
   imports: [
     HttpClientModule,
     CommonModule,
-    MaterialModule,
+    SharedModule,
     ReactiveFormsModule,
     FormsModule,
-    RouterModule.forChild(BooksRoutes)
+    RouterModule.forChild(BooksRoutes),
   ],
   exports: [],
   declarations: [
     BooksOverviewComponent,
     CreateBookComponent,
     BookListComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
   ],
-  providers: []
+  providers: [],
 })
 export class BooksModule {}
